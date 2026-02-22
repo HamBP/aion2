8453ae36dd1fb86fbd2c9c098b98209b48d4b188
algosketch <algosketch@gmail.com>
2026-02-23 07:11:59 +0900

    feat: 영웅 DB 일단 대충 넣기

--- composeApp/src/webMain/kotlin/me/algosketch/aioninfo/data/enhancement/EnhancementRepository.kt
+++ composeApp/src/webMain/kotlin/me/algosketch/aioninfo/data/enhancement/EnhancementRepository.kt
@@ -72,6 +72,92 @@ object EnhancementRepository {
                     kina = 1530000,
                 ),
             ),
+        ),
+        // TODO: 2~10 강 DB 만들기
+        ItemEnhancementInfo(
+            itemLevel = 108,
+            enhancementCosts = listOf(
+                EnhancementCost(
+                    stones = 540,
+                    kina = 13500,
+                ),
+                EnhancementCost(
+                    stones = 770,
+                    kina = 19300,
+                ),
+                EnhancementCost(
+                    stones = 1410,
+                    kina = 35300,
+                ),
+                EnhancementCost(
+                    stones = 0,
+                    kina = 0,
+                ),
+                EnhancementCost(
+                    stones = 0,
+                    kina = 0,
+                ),
+                EnhancementCost(
+                    stones = 0,
+                    kina = 0,
+                ),
+                EnhancementCost(
+                    stones = 3890,
+                    kina = 98000,
+                ),
+                EnhancementCost(
+                    stones = 4760,
+                    kina = 119000,
+                ),
+                EnhancementCost(
+                    stones = 5670,
+                    kina = 142000,
+                ),
+                EnhancementCost(
+                    stones = 6650,
+                    kina = 167000,
+                ),
+                EnhancementCost(
+                    stones = 9860,
+                    kina = 380000,
+                ),
+                EnhancementCost(
+                    stones = 11230,
+                    kina = 570000,
+                ),
+                EnhancementCost(
+                    stones = 12660,
+                    kina = 910000,
+                ),
+                EnhancementCost(
+                    stones = 14150,
+                    kina = 1420000,
+                ),
+                EnhancementCost(
+                    stones = 15690,
+                    kina = 1970000,
+                ),
+                EnhancementCost(
+                    stones = 17280,
+                    kina = 2160000,
+                ),
+                EnhancementCost(
+                    stones = 18930,
+                    kina = 3200000,
+                ),
+                EnhancementCost(
+                    stones = 20620,
+                    kina = 3500000,
+                ),
+                EnhancementCost(
+                    stones = 22370,
+                    kina = 5600000,
+                ),
+                EnhancementCost(
+                    stones = 24150,
+                    kina = 6100000,
+                ),
+            ),
         )
     )
 
